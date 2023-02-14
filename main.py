bde0bce4eb5101128126c5c05dde069c1738973b
Manvir Mann <manvirm29@gmail.com>
2023-02-13 23:37:08 -0800

    Download model

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import nltk
 from textblob import TextBlob
 from newspaper import article
 
+# Download model
+nltk.download('punkt')
+
 url = ''
 
 # Pass url into article, creating an article object focused on url
